32f5b92cec8a3799a42dd53d4a3cae9f8cac9087
Edward <127128556+Everest35@users.noreply.github.com>
2024-05-13 14:56:35 +0800

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,7 @@
+exports.quotes = [
+    'The only way to do great work is to love what you do. – Steve Jobs',
+    'Life is not about finding yourself, it’s about creating yourself. – George Bernard Shaw',
+    'Success is not the key to happiness. Happiness is the key to success. – Albert Schweitzer',
+    'The best way to predict the future is to invent it. – Alan Kay',
+    'Believe you can and you’re halfway there. – Theodore Roosevelt'
+];
